4aa2fb7aaac62a8f5a5ee526d46ea25b9566a84d
Vitaliy Voloshyn <vitaliy.srt1985@gmail.com>
2022-04-30 20:26:48 +0300

    x

---
+++ bboard/views.py
@@ -0,0 +1,10 @@
+from unittest import loader
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Bb
+
+
+def index(request):
+    bbs = Bb.objects.all()
+    return render(request, 'bboard/index.html', {'bbs': bbs})
